d1383ce5ec54ccd4d7cc2d782c47a7075fe99023
Kevin Wu <kevin.wu@live.com.my>
2017-12-02 00:56:45 +0800

    luis dialog 101

---
+++ LuisBot101/LuisBot101/Dialogs/LuisDialog.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace LuisBot101.Dialogs
+{
+    [LuisModel("modelID", "subscriptionID")]
+    [Serializable]
+    public class LuisDialog : LuisDialog<object>
+    {
+        [LuisIntent("None")]
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("None Intent Detected");
+            context.Wait(this.MessageReceived);
+        }
+        
+        [LuisIntent("Reminder.Find")]
+        public async Task ReminderFind(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Reminder.Find Intent Detected");
+            context.Wait(this.MessageReceived);
+        }
+
+        
+        [LuisIntent("Reminder.Create")]
+        public async Task ReminderCreate(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Reminder.Create Intent Detected");
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("Places.GetRoute")]
+        public async Task PlacesGetRoute(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Places.GetRoute Intent Detected");
+            context.Wait(this.MessageReceived);
+        }
+
+    }
+}
---
+++ LuisBot101/LuisBot101/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LuisBot101.WebApiApplication" Language="C#" %>
